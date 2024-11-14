194ddeb5365b3a0031c090d5b6f2215af19ce811
Mario Perna <pernamario.bus@gmail.com>
2024-11-14 08:18:57 +0100

    WIP WebSocket (From documentation https://socket.io/how-to/use-with-react)

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,23 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+getOrigin = () => {
+  if (process.env.NODE_ENV === 'production') {
+    if (process.env.FRONTEND_URL) {
+      return process.env.FRONTEND_URL
+    }
+    throw new Error('FRONTEND_URL is not set');
+  } else {
+    return 'http://localhost:5173';
+  }
+}
+
+const io = new Server(server, {
+  cors: {
+    origin: getOrigin(),
+  }
+});
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { socket } from './socket';
+import { ConnectionState } from './components/ConnectionState';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    // function onFooEvent(value: any) {
+    //   setFooEvents(previous => [...previous, value]);
+    // }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    // socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      // socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+  
+
 
   return (
     <>
-      <h1>UNIPEGASO - PROJECT WORK</h1>
+      <ConnectionState isConnected={ isConnected } />
     </>
   )
 }
