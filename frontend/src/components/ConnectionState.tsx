034e6611d676d65337adacbaeefd280e365d4b86
Mario Perna <pernamario.bus@gmail.com>
2024-12-20 17:42:23 +0100

    [Add] tw styles to websocket

--- frontend/src/components/ConnectionState.tsx
+++ frontend/src/components/ConnectionState.tsx
@@ -1,3 +1,15 @@
 export function ConnectionState({ isConnected }: { isConnected: boolean }) {
-  return <p>State: { '' + isConnected }</p>;
+  return (
+    <div className="flex">
+      <div className={"mr-2"}>
+        Websocket Status
+      </div>
+      <div id="connection-state" className={"flex items-center"}>
+        <span className={"relative flex h-3 w-3"}>
+          <span title={isConnected ? "Operational" : "Disconnected"} className={"animate-ping absolute inline-flex h-full w-full rounded-full  opacity-75 " + (isConnected ? "bg-green-400" : "bg-red-400")}></span>
+          <span className={"relative inline-flex rounded-full h-3 w-3 bg-green-500 " + (isConnected ? "bg-green-400" : "bg-red-400")}></span>
+        </span>
+      </div>
+    </div>
+  );
 }
