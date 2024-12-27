6f52a0113907677b51f02030fb2c6338a191c666
Mario Perna <pernamario.bus@gmail.com>
2024-12-27 19:34:42 +0100

    Add error handling for undefined APP_BACKEND_URL in socket configuration

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -3,5 +3,8 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // da implementare controllo su variabile di ambiente
 
-const APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL as string || 'http://localhost:3000';
+const APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL as string;
+if (!APP_BACKEND_URL) {
+  throw new Error('APP_BACKEND_URL is not defined');
+}
 export const socket = io(APP_BACKEND_URL);
