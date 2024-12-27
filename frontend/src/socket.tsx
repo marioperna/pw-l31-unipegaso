05d4bdc1dc7ca17279babdd940016603f4a6d75b
Mario Perna <pernamario.bus@gmail.com>
2024-12-27 19:23:05 +0100

    Refactor APP_BACKEND_URL to use import.meta.env for environment variable access in socket and Vite config

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
-import { APP_BACKEND_URL } from './app.env';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // da implementare controllo su variabile di ambiente
 
+const APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL as string || 'http://localhost:3000';
 export const socket = io(APP_BACKEND_URL);
