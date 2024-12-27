3526cb96586381ac13760a5c07f1648563bb9029
Mario Perna <pernamario.bus@gmail.com>
2024-12-27 14:11:27 +0100

    [Update] Italian localization file and extend Tailwind CSS color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,11 @@ export default {
       colors: {
         customPurple: "#8884d8",
         customGreen: "#82ca9d",
-        customYellow: "#ffc658"
+        customYellow: "#ffc658",
+        blueGray: "#6699CC",
+        realMadridYellow: "#FEBE10",
+        lavender: '#E6E6FA'
+
       },
     },
   },
