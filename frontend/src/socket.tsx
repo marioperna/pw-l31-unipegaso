10a523f189a7b018baa8afad77a7cd052397a17a
Mario Perna <pernamario.bus@gmail.com>
2024-12-28 10:35:40 +0100

    Update socket connection to use default backend URL if not defined

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -3,12 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // da implementare controllo su variabile di ambiente
 
-const APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL as string;
 
-if (!APP_BACKEND_URL) {
-  throw new Error('VITE_APP_BACKEND_URL is not defined');
-}
-
-console.log('VITE_APP_BACKEND_URL:', APP_BACKEND_URL);
-
-export const socket = io(APP_BACKEND_URL);
+const VITE_APP_BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL || 'http://localhost:3000';
+console.log('VITE_APP_BACKEND_URL:', VITE_APP_BACKEND_URL);
+export const socket = io(VITE_APP_BACKEND_URL);
